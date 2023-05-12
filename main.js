68428e3d7c2721c4e33e93b03f94cec500a9bd3f
Daniele Dequi <113129822+danieledequi@users.noreply.github.com>
2023-05-12 16:52:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert ('oi mundo');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelectorAll('.tecla')
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
